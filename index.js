3d747fa9bb4278897e95cf78baac93647c1eb9de
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-03-26 20:06:59 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const obj = record.find(function (element) {
+        return element.result === "W";
+    })
+    return obj != undefined ? obj.year : undefined;
+}
